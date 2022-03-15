94d65692a1e771cb4c3b1d39a2fa7a4cb05ae81e
Mahdi Parivash <mahdiparivsh@protonmail.com>
2022-03-15 09:44:42 +0330

    Sidebar Section Complate

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import "../styles/Sidebar.css";
+import {
+  BsFillRssFill,
+  BsFillBookmarkFill,
+  BsFillChatLeftFill,
+  BsCameraVideoFill,
+  BsFillQuestionCircleFill,
+  BsBriefcaseFill,
+  BsCalendarDateFill,
+} from "react-icons/bs";
+import { FaUserGraduate } from "react-icons/fa";
+import { HiUserGroup } from "react-icons/hi";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+            <BsFillRssFill className="siderbarIcon" />
+            <span className="sidebarListItemFeed">اخبار</span>
+          </li>
+          <li className="sidebarListItem">
+            <BsFillChatLeftFill className="siderbarIcon" />
+            <span className="sidebarListItemFeed">گفت و گو</span>
+          </li>
+          <li className="sidebarListItem">
+            <BsCameraVideoFill className="siderbarIcon" />
+            <span className="sidebarListItemFeed">فیلم ها</span>
+          </li>
+          <li className="sidebarListItem">
+            <HiUserGroup className="siderbarIcon" />
+            <span className="sidebarListItemFeed">گروه ها</span>
+          </li>
+          <li className="sidebarListItem">
+            <BsFillBookmarkFill className="siderbarIcon" />
+            <span className="sidebarListItemFeed">نشان شده ها </span>
+          </li>
+          <li className="sidebarListItem">
+            <BsFillQuestionCircleFill className="siderbarIcon" />
+            <span className="sidebarListItemFeed">سوالات</span>
+          </li>
+          <li className="sidebarListItem">
+            <BsBriefcaseFill className="siderbarIcon" />
+            <span className="sidebarListItemFeed">شغل ها</span>
+          </li>
+          <li className="sidebarListItem">
+            <BsCalendarDateFill className="siderbarIcon" />
+            <span className="sidebarListItemFeed">رویدادها</span>
+          </li>
+          <li className="sidebarListItem">
+            <FaUserGraduate className="siderbarIcon" />
+            <span className="sidebarListItemFeed">آموزش ها</span>
+          </li>
+        </ul>
+        <button className="sidebarButton">دیدن موارد بیشتر</button>
+        <hr className="sidebarHr" />
+        <ul className="sidebarFriendList">
+          <li className="sidebarFriend">
+            <img
+              className="siderbarFrindImg"
+              src="/assets/person/2.jpeg"
+              alt=""
+            />
+            <span className="sidebarFriendName">مهدی پریوش</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="siderbarFrindImg"
+              src="/assets/person/2.jpeg"
+              alt=""
+            />
+            <span className="sidebarFriendName">مهدی پریوش</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
